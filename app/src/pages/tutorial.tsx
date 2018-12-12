f8f9b47582ff3ce7b57e30bf1d6b46588582946c
matsudaibuki <matsuda.ibuki@revamp.co.jp>
2018-12-12 17:11:39 +0900

    first commit

---
+++ app/src/pages/tutorial.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export class Tutorial extends React.Component {
+    render() {
+        return(
+            <p>Hello,World</p>
+        )
+    }
+}
+
+export default Tutorial
