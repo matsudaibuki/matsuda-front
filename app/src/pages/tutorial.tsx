6837db8ef9f0b6db3e9a4d9c6cdcd3557ea9f101
matsudaibuki <matsuda.ibuki@revamp.co.jp>
2018-12-13 10:03:04 +0900

    master

--- app/src/pages/tutorial.tsx
+++ app/src/pages/tutorial.tsx
@@ -4,11 +4,18 @@ import { Row, Col } from 'antd'
 export class Tutorial extends React.Component {
     render() {
         return(
+<<<<<<< HEAD
             <Row>
                 <Col span={8}>
                     <p>Hello,World</p>
                 </Col>
             </Row>
+=======
+            <div>
+                <p>Hello,World</p>
+                <p>master</p>
+            </div>
+>>>>>>> master
         )
     }
 }
