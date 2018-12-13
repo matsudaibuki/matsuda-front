aed038f344e1a711e953ec7565afbeb3c60f5e6b
matsudaibuki <matsuda.ibuki@revamp.co.jp>
2018-12-13 11:05:53 +0900

    commit

--- app/src/pages/tutorial.tsx
+++ app/src/pages/tutorial.tsx
@@ -1,21 +1,13 @@
 import * as React from 'react'
-import { Row, Col } from 'antd'
 
 export class Tutorial extends React.Component {
     render() {
         return(
-<<<<<<< HEAD
-            <Row>
-                <Col span={8}>
-                    <p>Hello,World</p>
-                </Col>
-            </Row>
-=======
             <div>
+                <p>Hello,World</p>
                 <p>Hello,World</p>
                 <p>master</p>
             </div>
->>>>>>> master
         )
     }
 }
